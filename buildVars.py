fcada70f987b059f8f98f26659a5dbffd3fd97b1
Christopher Toth <q.alpha@gmail.com>
2023-04-15 14:17:58 -0600

    Adding an upload on tag action and bumping version for latest NVDA

--- buildVars.py
+++ buildVars.py
@@ -19,17 +19,17 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Allows copying items from the review cursor to the clipboard"""),
 	# version
-	"addon_version" : "1.1",
+	"addon_version" : "1.2",
 	# Author(s)
 	"addon_author" : u"Tyler Spivey <tspivey@pcdesk.net>",
 	# URL for the add-on documentation support
-	"addon_url" : "https://github.com/tspivey/virtualCopy",
+	"addon_url" : "https://github.com/ctoth/virtualCopy",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2019.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2022.1",
+	"addon_lastTestedNVDAVersion": "2023.1",
 	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
 	"addon_updateChannel" : None,
 }
